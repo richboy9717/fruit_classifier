a8c22af20cd4b4080a0289194015b399bf92f0bf
Raximov Ilhomjon <98205793+richboy9717@users.noreply.github.com>
2022-05-28 13:10:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from fastai.vision.all import *
 import pathlib
 import plotly.express as px
-import tkinter as Tk
+from tkinter import *
 
 st.title("Fruits Classifier")
 file = st.file_uploader("Rasm yuklash", type=["jpg", "png", "jpeg", "ljpg", 'fjpg'])
